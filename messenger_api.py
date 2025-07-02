11a82852e890197bb8ef22d6a70a1a7318e9e6c0
Marco Schmalz <marco@schess.ch>
2025-07-02 09:50:21 +0200

    final api

--- messenger_api.py
+++ messenger_api.py
@@ -1,10 +1,13 @@
-from api_utils import API, run, NotFound, UnprocessableEntity
+from api_utils import API, run, NotFound, UnprocessableEntity, PubSub
 
 api = API()
 
+broker = PubSub()
+
 messages = [
-    {"username": "isa", "message": "tschou zäme"},
-    {"username": "piotr", "message": "salü isa!"},
+    {"sender": "piotr", "receiver": "", "content": "", "type": "new-user"},
+    {"sender": "isa", "receiver": "", "content": "", "type": "new-user"},
+    {"sender": "isa", "receiver": "piotr", "content": "hallo piotr", "type": "message"},
 ]
 
 @api.GET("/api/chat/")
@@ -12,7 +15,13 @@ def list_messages():
     return messages
 
 @api.POST("/api/chat/")
-def post_message(username:str, message: str):
-    messages.append({"username": username, "message": message})
+def post_message(sender:str, receiver:str="", content:str="", type:str="message"):
+    msg = {"sender": sender, "receiver": receiver, "content": content, "type": type}
+    messages.append(msg)
+    broker.publish("message", msg)
+
+@api.GET("/api/chat/stream")
+def message_stream(request):
+    return broker.streaming_response(request)
 
 run(api, port=5000)
